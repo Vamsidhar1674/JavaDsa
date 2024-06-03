0a71d27e061f1a48e84fe1c5ccbf88928c8e9cb6
Vamsidhar1674 <vamsidharmallangi@gmail.com>
2024-06-03 12:04:11 +0530

    Insertion Sort
    Updated Insertion Sort

--- Sorting/insertionSort.java
+++ Sorting/insertionSort.java
@@ -1,7 +1,26 @@
 public class insertionSort {
     public static void main(String[] args) {
-        int[] array={}
+        int[] array={21,51,52,54,84,89};
+        sort(array);
+        for(Integer i:array){
+            System.out.println(i);
+        }
         
+    }
+    static void sort(int[]array){
+        for(int i=1;i<array.length;i++){
+            int curr=array[i];
+            int prev= i-1;
+            while(prev>=0 && array[prev]>curr){
+                array[prev+1]=array[prev];
+                prev--;
+
+
+            }
+            array[prev+1]=curr;
+
+        }
+
     }
     
 }
